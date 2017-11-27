c9b6f154e2f5cb103bf76b9424f4d36e370ac5c8
Faisal Siddiqi <>
2017-11-27 17:11:40 -0500

    dotenv

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
@@ -13,21 +14,15 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 // uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-
-// Configure CORS
+// app.use(favicon(path.join(__dirname, 'public', 'favicon.ico'))); Configure
+// CORS
 var corsOptions = {
   exposedHeaders: 'x-total-count'
 }
 app.use(cors(corsOptions));
 app.use(logger('dev'));
-app.use(bodyParser.json({
-  limit: '50mb'
-}));
-app.use(bodyParser.urlencoded({
-  limit: '50mb',
-  extended: false
-}));
+app.use(bodyParser.json({limit: '50mb'}));
+app.use(bodyParser.urlencoded({limit: '50mb', extended: false}));
 
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
@@ -39,36 +34,33 @@ app.use('/', routes);
 /** Add API individual routes here */
 app.use('/Tasks', Tasks);
 app.use('/Articles', Articles);
-// app.use('/stations', Stations);
-
-// catch 404 and forward to error handler
+// app.use('/stations', Stations); catch 404 and forward to error handler
 app.use(function (req, res, next) {
   var err = new Error('Not Found');
   err.status = 404;
   next(err);
 });
 
-// development error handler
-// will print stacktrace
+// development error handler will print stacktrace
 if (app.get('env') === 'development') {
-  app.use(function (err, req, res, next) {
+  app
+    .use(function (err, req, res, next) {
+      res.status(err.status || 500);
+      res.render('error', {
+        message: err.message,
+        error: err
+      });
+    });
+}
+
+// production error handler no stacktraces leaked to user
+app
+  .use(function (err, req, res, next) {
     res.status(err.status || 500);
     res.render('error', {
       message: err.message,
-      error: err
+      error: {}
     });
   });
-}
-
-// production error handler
-// no stacktraces leaked to user
-app.use(function (err, req, res, next) {
-  res.status(err.status || 500);
-  res.render('error', {
-    message: err.message,
-    error: {}
-  });
-});
-
 
 module.exports = app;
--- dbconnection.js
+++ dbconnection.js
@@ -1,11 +1,9 @@
 var mysql = require('mysql');
-var connection = mysql.createPool({
-
-    host: 'localhost',
-    user: 'fs',
-    password: 'fs',
-    database: 'fstest'
-
-
-});
+var connstr = {
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE
+};
+var connection = mysql.createPool(connstr);
 module.exports = connection;
